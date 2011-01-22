0f3eeecb8c534261271a0502e19406ca90f710c8
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2011-01-22 21:03:48 +0800

    * tweak order and words

--- fix-epub.py
+++ fix-epub.py
@@ -5,26 +5,19 @@ import zipfile
 import tempfile
 import shutil
 
-lang = "zh-CN"
+# if your case is not zh-CN, change it here
+LANG = "zh-CN"
+# and yes, it's "zh-CN", NOT "zh_CN"
 
 tag = "xml:lang"
 html_re = re.compile(r'(\<html[^\>]*)\>')
-replace = r'\1 ' + tag + r'="' + lang + r'" >'
+replace = r'\1 %s="%s">' % (tag, LANG)
 
-try :
-	epub = sys.argv[1]
-except IndexError :
-	print "Usage: \"%s <filename.epub>\" to fix filename.epub, get filename.fix.epub" % sys.argv[0]
-	sys.exit()
-
-tmpdir = tempfile.mkdtemp()
-logfile = os.fdopen(2, 'w')	# use stderr
-
-def addZip(archive, base) :
-	dirname = os.path.join(tmpdir, base)
+def addZip(archive, base, curdir) :
+	dirname = os.path.join(base, curdir)
 	for f in os.listdir(dirname) :
-		filename = os.path.join(base, f)
-		fullname = os.path.join(tmpdir, filename)
+		filename = os.path.join(curdir, f)
+		fullname = os.path.join(base, filename)
 		if os.path.isfile(fullname) :
 			if f.lower().endswith("html") or f.lower().endswith("xml") :
 				[tmpFile, tmpName] = tempfile.mkstemp()
@@ -46,7 +39,16 @@ def addZip(archive, base) :
 				os.rename(tmpName, fullname)
 			archive.write(fullname, filename)
 		elif os.path.isdir(fullname) :
-			addZip(archive, filename)
+			addZip(archive, base, filename)
+
+try :
+	epub = sys.argv[1]
+except IndexError :
+	print "Usage: \"%s <filename.epub>\" to fix filename.epub, get filename.fix.epub" % sys.argv[0]
+	sys.exit()
+
+tmpdir = tempfile.mkdtemp()
+logfile = os.fdopen(2, 'w')	# use stderr
 
 epubFile = zipfile.ZipFile(epub, "r")
 epubFile.extractall(tmpdir)
@@ -54,7 +56,7 @@ epubFile.close()
 
 epub = re.sub(r'(.*)\.epub', r'\1.fix.epub', epub)
 epubFile = zipfile.ZipFile(epub, "w", zipfile.ZIP_DEFLATED)
-addZip(epubFile, '.')
+addZip(epubFile, tmpdir, '.')
 epubFile.close()
 
 shutil.rmtree(tmpdir)
