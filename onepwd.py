0973928875f1d41a46920a9be5662f1b611cc835
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2011-06-26 15:52:19 +0800

    + onepwd.py

---
+++ onepwd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import re, getpass
+import hmac, hashlib, base64
+
+try :
+	msg = sys.argv[1]
+except IndexError :
+	sys.stderr.write('  sitekey: ')
+	msg = sys.stdin.readline().rstrip()
+
+key = getpass.getpass('masterkey: ', sys.stderr)
+pwd = base64.b64encode(hmac.new(key, msg, hashlib.md5).digest()).rstrip('=').replace('+', '').replace('/', '')
+
+sys.stderr.write('\n')
+sys.stdout.write(pwd[:20])
+sys.stdout.flush()
+sys.stderr.write('\n')
