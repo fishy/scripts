f68fda0526d7e9852c491330036704b50d49cd7f
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2011-12-04 14:55:42 +0800

    * rename after resize and change target filename.

--- resize-for-picasa.sh
+++ resize-for-picasa.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 size="2048x2048"
-append="_resize"
+prepend="resize_"
 ext="jpg"
 
 for file in *.${ext}; do
-	already=`echo "${file}" | grep "${append}"`
+	already=`echo "${file}" | grep "${prepend}"`
 	if [ -n "${already}" ]; then
 		echo "skipping ${file} ..."
 	else
-		target=${file%.${ext}}${append}.${ext}
+		target=${prepend}${file}
 		echo "resizing ${file} to ${target} ..."
 		convert -resize ${size} ${file} ${target}
+		echo "renaming ${target} to ${file} ..."
+		mv ${target} ${file}
 	fi
 done
